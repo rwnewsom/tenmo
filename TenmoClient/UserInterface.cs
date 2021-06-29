4fa0e67df2eb970a0ac5a7647d1c0b398dcd0a61
Matt Eland <matt.eland@gmail.com>
2021-06-29 16:02:50 -0400

    Ported over cohort 12 project.

---
+++ TenmoClient/UserInterface.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using TenmoClient.Data;
+
+namespace TenmoClient
+{
+    public class UserInterface
+    {
+        private readonly ConsoleService consoleService = new ConsoleService();
+        private readonly AuthService authService = new AuthService();
+
+        private bool shouldExit = false;
+
+        public void Start()
+        {
+            while (!shouldExit)
+            {
+                while (!authService.IsLoggedIn)
+                {
+                    ShowLogInMenu();
+                }
+
+                // If we got here, then the user is logged in. Go ahead and show the main menu
+                ShowMainMenu();
+            }
+        }
+
+        private void ShowLogInMenu()
+        {
+            Console.WriteLine("Welcome to TEnmo!");
+            Console.WriteLine("1: Login");
+            Console.WriteLine("2: Register");
+            Console.Write("Please choose an option: ");
+
+            if (!int.TryParse(Console.ReadLine(), out int loginRegister))
+            {
+                Console.WriteLine("Invalid input. Please enter only a number.");
+            }
+            else if (loginRegister == 1)
+            {
+                HandleUserLogin();
+            }
+            else if (loginRegister == 2)
+            {
+                HandleUserRegister();
+            }
+            else
+            {
+                Console.WriteLine("Invalid selection.");
+            }
+        }
+
+        private void ShowMainMenu()
+        {
+            int menuSelection = -1;
+            while (menuSelection != 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Welcome to TEnmo! Please make a selection: ");
+                Console.WriteLine("1: View your current balance");
+                Console.WriteLine("2: View your past transfers");
+                Console.WriteLine("3: View your pending requests");
+                Console.WriteLine("4: Send TE bucks");
+                Console.WriteLine("5: Request TE bucks");
+                Console.WriteLine("6: Log in as different user");
+                Console.WriteLine("0: Exit");
+                Console.WriteLine("---------");
+                Console.Write("Please choose an option: ");
+
+                if (!int.TryParse(Console.ReadLine(), out menuSelection))
+                {
+                    Console.WriteLine("Invalid input. Please enter only a number.");
+                }
+                else
+                {
+                    switch (menuSelection)
+                    {
+                        case 1:
+                            Console.WriteLine("NOT IMPLEMENTED!"); // TODO: Implement me
+                            break;
+                        case 2:
+                            Console.WriteLine("NOT IMPLEMENTED!"); // TODO: Implement me
+                            break;
+                        case 3:
+                            Console.WriteLine("NOT IMPLEMENTED!"); // TODO: Implement me
+                            break;
+                        case 4:
+                            Console.WriteLine("NOT IMPLEMENTED!"); // TODO: Implement me
+                            break;
+                        case 5:
+                            Console.WriteLine("NOT IMPLEMENTED!"); // TODO: Implement me
+                            break;
+                        case 6:
+                            Console.WriteLine();
+                            UserService.SetLogin(new API_User()); //wipe out previous login info
+                            return;
+                        default:
+                            Console.WriteLine("Goodbye!");
+                            shouldExit = true;
+                            return;
+                    }
+                }
+            }
+        }
+
+        private void HandleUserRegister()
+        {
+            bool isRegistered = false;
+
+            while (!isRegistered) //will keep looping until user is registered
+            {
+                LoginUser registerUser = consoleService.PromptForLogin();
+                isRegistered = authService.Register(registerUser);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Registration successful. You can now log in.");
+        }
+
+        private void HandleUserLogin()
+        {
+            while (!UserService.IsLoggedIn) //will keep looping until user is logged in
+            {
+                LoginUser loginUser = consoleService.PromptForLogin();
+                API_User user = authService.Login(loginUser);
+                if (user != null)
+                {
+                    UserService.SetLogin(user);
+                }
+            }
+        }
+    }
+}
